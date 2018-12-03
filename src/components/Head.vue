a81d52dc52120eb10cd22ed4ce1413d69f5681f4
liugangfeng <liugangfeng@henrongyi.com>
2018-12-03 18:59:25 +0800

    .

--- src/components/Head.vue
+++ src/components/Head.vue
@@ -1,18 +1,20 @@
 <template>
 <div>
     <div class="nav">
-      <div>
-        <router-link tag="a" to="/">返回首页</router-link>
-        <router-link tag="a" to="/register">会员注册</router-link>
-        <router-link tag="a" to="/login">会员登录</router-link>
-      </div>
-      <div>
-        <router-link tag="a" to="/allianceintroduction">联盟概况</router-link>
-        <router-link to="/alliancedynamics">联盟动态</router-link>
-        <router-link to="/selfassessment">企业自评</router-link>
-        <router-link to="/IndustryMap">产业地图</router-link>
-        <router-link to="/contactus">联系我们</router-link>
-      </div>                
+      <div class="nav-whole">
+        <div class="nav-login">
+          <router-link tag="a" to="/">返回首页</router-link>
+          <router-link tag="a" to="/register">会员注册</router-link>
+          <router-link tag="a" to="/login">会员登录</router-link>
+        </div>
+        <div class="nav-main">
+          <router-link tag="a" to="/allianceintroduction">联盟概况</router-link>
+          <router-link tag="a" to="/alliancedynamics">联盟动态</router-link>
+          <router-link tag="a" to="/selfassessment">企业自评</router-link>
+          <router-link tag="a" to="/IndustryMap">产业地图</router-link>
+          <router-link tag="a" to="/contactus">联系我们</router-link>
+        </div>
+      </div>               
     </div>
     <div class="logo"></div>
     </div>
@@ -22,17 +24,41 @@
   background-color: #006699;
   height: 100px;
   a {
-    color: #ffffff;
+    color: #dddddd;
     text-decoration: none;
     padding: 5px;
   }
   a:hover {
-    color: red;
+    color: gold;
   }
+}
+.nav-whole {
+  margin: auto;
+  width: 1200px;
+}
+
+.nav .nav-login {
+  padding: 10px;
+  height: 40px;
+  color: #999999;
+  font-size: 14px;
+  line-height: 40px;
+  text-align: right;
+}
+.nav .nav-main {
+
+  color: #eeeeee;
+  font-size: 20px;
+  text-align: right;
+  a {
+    padding: 0 10px;
+  }
+}
+.nav .nav-main a {
 }
 .logo {
   height: 150px;
-  background-color: red;
+  background-color: gold;
   background: url("../assets/logo.png") no-repeat 0% 0%;
 }
 </style>
